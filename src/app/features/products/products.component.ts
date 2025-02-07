5570f3baa21dd32b9e587b13a9c33a0c3ed7c612
Guilherme Oliveira Fernandes <gui_olivernandes@hotmail.com>
2025-02-07 15:41:02 -0300

    Sei lá pai

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -19,6 +19,16 @@ export class ProductsComponent {
   constructor(private modalService: BsModalService) {}
 
   openModal(template: TemplateRef<any>) {
+    const closeInterceptor = () => {
+      this.confirmPromise = new Promise((resolve, reject) => {
+        this.confirmResolve = resolve;
+        this.confirmReject = reject;
+      });
+      this.confirmModalRef = this.modalService.show(template, { class: 'modal-lg' });
+ 
+      return this.confirmPromise;
+    };
+
     this.bsModalRef = this.modalService.show(template, {
       class: 'modal-lg',
     });
@@ -38,7 +48,7 @@ export class ProductsComponent {
  
       return this.confirmPromise;
     };
-    this.bsModalRef = this.modalService.show(ModalContentWithInterceptorComponent, { closeInterceptor });
+    this.bsModalRef = this.modalService.show(ModalContentWithInterceptorComponent, { closeInterceptor, class: 'modal-lg' });
     this.bsModalRef.content.closeBtnName = 'Close';
   }
  
@@ -58,45 +68,45 @@ export class ProductsComponent {
 }
 
 @Component({
-  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'modal-content-with-interceptor',
   template: `
-    <div class="modal-overlay">
-      <div class="modal-container">
-        <button
-          type="button"
-          class="close-btn"
-          aria-label="Close"
-          (click)="bsModalRef?.hide()"
-        >
-          &times;
-        </button>
-        <div class="modal-content">
-          <img
-            src="assets/images/004591fb67cf8816eb223c36a0cef2c4.jpg"
-            alt="Aguarrás"
-            class="modal-image"
-          />
-          <div class="modal-details">
-            <h3>Aguarrás</h3>
-            <p><strong>Indicação de uso:</strong></p>
-            <p>
-              Indicado na diluição de esmaltes sintéticos longos em óleo,
-              desengraxante e desengordurante.
-            </p>
-            <label for="size">Tamanho:</label>
-            <select id="size">
-              <option value="">----</option>
-              <option value="5L">5L</option>
-              <option value="10L">10L</option>
-            </select>
-            <button class="btn btn-primary">Solicitar Orçamento</button>
-          </div>
-        </div>
+    <div class="modal-header">
+      <button
+        type="button"
+        class="close-btn"
+        aria-label="Close"
+        (click)="bsModalRef?.hide()"
+      >
+        &times;
+      </button>
+    </div>
+
+    <div class="modal-body">
+      <img
+        src="assets/images/004591fb67cf8816eb223c36a0cef2c4.jpg"
+        alt="Aguarrás"
+        class="modal-image"
+      />
+      <div class="modal-details">
+        <h3>Aguarrás</h3>
+        <p><strong>Indicação de uso:</strong></p>
+        <p>
+          Indicado na diluição de esmaltes sintéticos longos em óleo,
+          desengraxante e desengordurante.
+        </p>
+        <label for="size">Tamanho:</label>
+        <select id="size">
+          <option value="">----</option>
+          <option value="5L">5L</option>
+          <option value="10L">10L</option>
+        </select>
       </div>
     </div>
+    <div class="modal-footer">
+      <button class="btn btn-primary">Adicionar ao Carrinho</button>
+    </div>
   `,
-  styleUrls: ['./products.component.scss'], // Caminho ajustado
+  styleUrls: ['./products.component.scss'],
   standalone: false
 })
 export class ModalContentWithInterceptorComponent {
